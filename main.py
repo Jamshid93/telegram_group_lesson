fd8533335870ce447e756277ca2cd4140c0868f4
Jamshid93 <hamzayevjf@gmail.com>
2022-11-25 20:38:14 +0500

    count user from json file

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ f=open("result.json").read()
 data=json.loads(f)
 # print(data['name'])
 messag=data['messages']
-count=0
+count_users=0
+user=[]
 for i in messag:
     if i['type']=='message':
-        count+=1
-print(count)
+        if user.count(i['from'])==0:
+            count_users+=1
+        user.append(i['from'])
+print(count_users)
+
  
