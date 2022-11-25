222599d1fe871b5e17c63e015b98196a3d67d52a
Jamshid93 <hamzayevjf@gmail.com>
2022-11-25 20:16:29 +0500

    count massage from file

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ f=open("result.json").read()
 data=json.loads(f)
 # print(data['name'])
 messag=data['messages']
+count=0
 for i in messag:
-    print(i['id'])
+    if i['type']=='message':
+        count+=1
+print(count)
+ 
